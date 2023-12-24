0f76f78f64f8296687e5b58a91daacffe7d88952
Valentin Rios <valentin.rios@accelone.com>
2023-12-24 18:24:41 -0500

    add fetch

--- main.js
+++ main.js
@@ -4,24 +4,22 @@ let mainPlayer = {
     level: localStorage.getItem("mainPlayerLevel")
 }
 
-let enemies = [
-    {
-        name: "Tuco Salamanca",
-        level: 1
-    },
-    {
-        name: "Mike Ehrmantraut",
-        level: 5
-    },
-    {
-        name: "Gustavo Fring",
-        level: 9
-    },
-    {
-        name: "Todd Alquist",
-        level: 15
-    },
-]
+let enemies = []
+const response = fetch("https://raw.githubusercontent.com/lcdovictor/files/main/enemies.json")
+                    .then(response => response.json())
+                    .then(data => {       
+                        enemies = data;                  
+                        document.getElementById("displayNameEnemyOne").innerText = enemies[0].name
+                        document.getElementById("displayNameEnemyTwo").innerText = enemies[1].name
+                        document.getElementById("displayNameEnemyThree").innerText = enemies[2].name
+                        document.getElementById("displayNameEnemyFour").innerText = enemies[3].name
+                        document.getElementById("displayLevelEnemyOne").innerText = enemies[0].level
+                        document.getElementById("displayLevelEnemyTwo").innerText = enemies[1].level
+                        document.getElementById("displayLevelEnemyThree").innerText = enemies[2].level
+                        document.getElementById("displayLevelEnemyFour").innerText = enemies[3].level
+                    })
+                    .catch(error => console.error(error));
+
 
 document.getElementById("displayLevel").innerText = mainPlayer.level
 if (!mainPlayer.level) {
@@ -31,14 +29,6 @@ let nameButton = document.getElementById("nameButton")
 nameButton.addEventListener("click", setName)
 let resetLevelButton = document.getElementById("resetLevelButton")
 resetLevelButton.addEventListener("click", resetLevel)
-document.getElementById("displayNameEnemyOne").innerText = enemies[0].name
-document.getElementById("displayNameEnemyTwo").innerText = enemies[1].name
-document.getElementById("displayNameEnemyThree").innerText = enemies[2].name
-document.getElementById("displayNameEnemyFour").innerText = enemies[3].name
-document.getElementById("displayLevelEnemyOne").innerText = enemies[0].level
-document.getElementById("displayLevelEnemyTwo").innerText = enemies[1].level
-document.getElementById("displayLevelEnemyThree").innerText = enemies[2].level
-document.getElementById("displayLevelEnemyFour").innerText = enemies[3].level
 
 function setName() {
     let username = document.getElementById("name").value
